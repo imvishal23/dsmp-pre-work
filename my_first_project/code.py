8534d1d404d835c9adf7efd80e85de0378598cd8
imvishal23 <imvishal23@users.noreply.github.com>
2020-04-04 20:10:33 +0530

    Code file created.

---
+++ my_first_project/code.py
@@ -0,0 +1,73 @@
+# --------------
+# Code starts here
+class_1 = ['Geoffrey Hinton', 'Andrew Ng', 'Sebastian Raschka', 'Yoshua Bengio']
+class_2 = ['Hilary Mason', 'Carla Gentry', 'Corinna Cortes']
+new_class = class_1 + class_2 
+print(new_class)
+new_class = new_class + ['Peter Warden']
+print(new_class) 
+del new_class[5]
+print(new_class)
+
+
+
+# Code ends here
+
+
+# --------------
+# Code starts here
+
+# Create the Dictionary
+
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+math = courses['Math']
+english = courses['English']
+history = courses['History']
+french = courses['French']
+science = courses['Science']
+
+# Store the all the subject in one variable `Total`
+total = math + english + history + french + science
+
+# Print the total
+print (total)
+
+# Insert percentage formula
+percentage = (total * 100 / 500)
+
+# Print the percentage
+print(percentage)
+# Code ends here
+
+
+# --------------
+# Code starts here
+
+# Create the Dictionary
+mathematics = {'Andrew Ng':95, 'Geoffrey Hinton': 78, 'Sebastian Raschka': 65, 'Yoshua Benjio':50, 'Hilary Mason':70, 'Cortinna Cortes':66, 'Peter Warden':75 }
+ 
+topper = max(mathematics,key = mathematics.get)
+print(topper)
+
+# Code ends here    
+
+
+# --------------
+# Given string
+topper = 'andrew ng'
+
+
+# Code starts here
+first_name = (topper.split()[0])
+last_name = (topper.split()[1])
+full_name = last_name + ' ' + first_name
+certificate_name = full_name.upper()
+print(certificate_name)
+
+# Code ends here
+
+
